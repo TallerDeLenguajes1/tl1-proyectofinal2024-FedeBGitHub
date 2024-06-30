9d7cebfc1444a775f8d421611d83dcd02298d8aa
FedeBGitHub <fedebarrionuevo76@gmail.com>
2024-06-30 19:09:40 -0300

    FabricaDePersonajes Terminado

--- FabricaDePersonajes.cs
+++ FabricaDePersonajes.cs
@@ -18,9 +18,9 @@ namespace fabricaDePersonajes
         {
             
             //Traigo todo el contenido de la api (dentro estan los personajes)
-            //DragonBall contenidoApi = await GetDragonBallAsync();
+            
             //Traigo un personaje aleatorio y lo guardo
-            Items P = contenidoApi.listaPersonajes[random.Next(0, 58)]; //Items es una subclase de DragonBall
+            Items P = contenidoApi.listaPersonajes[random.Next(0, 59)]; //Items es una subclase de DragonBall
             //Paso los datos relevantes
             Personaje personajeAleatorio = new Personaje();
             personajeAleatorio.Datos = new Datos();
@@ -29,11 +29,11 @@ namespace fabricaDePersonajes
             personajeAleatorio.Datos.Raza = P.Race;
             personajeAleatorio.Datos.Descripcion = P.Description;
 
-            personajeAleatorio.Caracteristicas.Velocidad = random.Next(1, 10);
-            personajeAleatorio.Caracteristicas.Destreza = random.Next(1, 5);
-            personajeAleatorio.Caracteristicas.Fuerza = random.Next(1, 10);
-            personajeAleatorio.Caracteristicas.Ki = random.Next(1, 10);
-            personajeAleatorio.Caracteristicas.Resistencia = random.Next(1, 10);
+            personajeAleatorio.Caracteristicas.Velocidad = random.Next(1, 11);
+            personajeAleatorio.Caracteristicas.Destreza = random.Next(1, 6);
+            personajeAleatorio.Caracteristicas.Fuerza = random.Next(1, 11);
+            personajeAleatorio.Caracteristicas.Ki = random.Next(1, 11);
+            personajeAleatorio.Caracteristicas.Resistencia = random.Next(1, 11);
             personajeAleatorio.Caracteristicas.Salud = 100;
             return personajeAleatorio;
         }
